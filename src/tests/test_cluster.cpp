7862df62113b775f6936cee42526728dd1eaaa07
JoshuaSBrown <joshbro42867@yahoo.com>
2018-09-13 08:27:31 -0600

    Updated the test file to relfect Riley's changes

--- src/tests/test_cluster.cpp
+++ src/tests/test_cluster.cpp
@@ -2,20 +2,29 @@
 #include <cassert>
 #include <vector>
 #include <memory>
+//#include <map>
+//#include <iterator>
 
 #include <kmccoursegrain/cluster.hpp>
 
 using namespace std;
 using namespace kmccoursegrain;
 
+#ifdef _E_
+#define Err 1
+#else
+#define Err 0
+#endif
+
 int main(void){
+
 
   cout << "Testing: Cluster constructor" << endl;
   {
     Cluster cl;
 
   }
-	
+
   cout << "Testing: Cluster identity setter" << endl;
   {
     Cluster cl;
@@ -36,8 +45,9 @@ int main(void){
       fail = true;
     }
     assert(fail);
-  }
-/*	if(Err==1) cout<<"Error Reporting On"<<endl;
+  }  
+/*
+	if(Err==1) cout<<"Error Reporting On"<<endl;
 	//Testing the Threshold Setter
 	{
 		cout<<"Testing Thresh"<<endl;
@@ -51,22 +61,66 @@ int main(void){
 		shared_ptr<cluster> tmp = make_shared<cluster>();
 		vector<double> nRates = {1.0,2.0};
 		vector<int> nId = {2,3};
-		int vFreq = 1;
-		int sId = 1;
+		map<int, double> neighbors1 = {
+			{2,1.0},
+			{3,2.0}
+		};
+		int vFreq1 = 1;
+		int sId1 = 1;
 
-		vector<double> nRates2 = {1.0,3.0};
+		vector<double> nRates2 = {1.0,5.0};
 		vector<int> nId2 = {1,3};
+		map<int, double> neighbors2 = {
+			{1,1.0},
+			{3,5.0}
+		};
 		int vFreq2 = 2;
 		int sId2 = 2;
+
+		vector<double> nRates3 = {2.0,5.0};
+		vector<int> nId3 = {1,2};
+		map<int, double> neighbors3 = {
+			{1,2.0},
+			{2,5.0}
+		};
+		int vFreq3 = 3;
+		int sId3 = 3;
 		//site site1(sId, nRates, nId, vFreq);
-		shared_ptr<site> site1(new site(sId,nRates,nId,vFreq));
-		shared_ptr<site> site2(new site(sId2,nRates2,nId2,vFreq2));
+		shared_ptr<site> site1(new site(sId1,vFreq1));
+		shared_ptr<site> site2(new site(sId2,vFreq2));
+		shared_ptr<site> site3(new site(sId3,vFreq3));
+		
+		map<shared_ptr<site>,double> linkSite1 = {
+			{site2,1.0},
+			{site3,2.0}
+		};
+
+		map<shared_ptr<site>,double> linkSite2 = {
+			{site1,1.0},
+			{site3,5.0},
+			{site2,10.0}
+		};
+
+		site1->addNeighbors(linkSite1);
+		site2->addNeighbors(linkSite2);
+	
+		site1->printInfo();
+		site2->printInfo();	
 
 		tmp->addSite(site1);
+		
 		tmp->addSite(site2);
-		tmp->printClusterInfo();
+		tmp->printInfo();
 		cout<<"Dwell Time: "<<tmp->dwellTime()<<endl;
-		cout<<"Prob Hop from site 1 to site 2: "<<tmp->probHop(site1,site2)<<endl;
-	}*/
+		cout<<"Prob Hop from site 1 to site 2: "<<site1->probHop(2)<<endl;
+		cout<<"Prob Hop from site 1 to site 2 (overloaded):"<<site1->probHop(site2)<<endl;
+	
+	//Testing Convergence
+	
+		tmp->convergence(5);
+		tmp->printInfo();
+		site1->printInfo();
+		site2->printInfo();
+	}	*/
 	return 0;
 }
