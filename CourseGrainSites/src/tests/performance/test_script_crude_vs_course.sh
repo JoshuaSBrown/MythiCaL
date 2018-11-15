81d0928db03f18fa0a7947f54de3c85fc5068ab1
JoshuaSBrown <joshbro42867@yahoo.com>
2018-11-15 12:02:13 -0700

    1. Added script for performance testing

--- CourseGrainSites/src/tests/performance/test_script_crude_vs_course.sh
+++ CourseGrainSites/src/tests/performance/test_script_crude_vs_course.sh
@@ -8,19 +8,23 @@ particles=3
 total_crude=0
 total_course=0
 
-number_of_seeds=2
-seed_max=$(( $number_of_seeds + 1))
-for seed in $(seq 1 $seed_max)
+number_of_seeds=1
+for seed in $(seq 1 $number_of_seeds)
 do
   ./performance_test_crude_vs_coursegrain $sigma $length $seed $resolution $particles > out.txt
-  value_crude=$(cat out.txt | grep "Crude Mone Carlo Run Time" | awk '{print $6}') 
+  value_crude=$(cat out.txt | grep "Crude Monte Carlo Run Time" | awk '{print $6}') 
+  echo $value_crude
   total_crude=$(( $value_crude+$total_crude ))
-  value_course=$(cat out.txt | grep "Course Mone Carlo Run Time" | awk '{print $6}') 
-  total_course=$(( $value_course+$total_course ))
+  value_course=$(cat out.txt | grep "Course Monte Carlo Run Time" | awk '{print $6}') 
+  echo $value_course
+  total_course=$(( $value_course+$total_course )) 
 done
 
-average_time_crude=$(bc <<< "scale = 6; ( $value_crude / $number_of_seeds )")
-average_time_course=$(bc <<< "scale = 6; ( $value_course / $number_of_seeds )")
+echo $total_crude
+echo $total_course
+
+average_time_crude=$(bc <<< "scale = 6; ( $total_crude / $number_of_seeds )")
+average_time_course=$(bc <<< "scale = 6; ( $total_course / $number_of_seeds )")
 
 echo $average_time_crude
 echo $average_time_course
