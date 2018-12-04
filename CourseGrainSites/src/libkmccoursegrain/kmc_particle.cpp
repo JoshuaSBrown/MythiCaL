700ac3aa52de10d123a8001ea1b5fafba249aee6
JoshuaSBrown <joshbro42867@yahoo.com>
2018-12-04 13:06:29 -0700

    Changed particle to walker

--- CourseGrainSites/src/libkmccoursegrain/kmc_particle.cpp
+++ CourseGrainSites/src/libkmccoursegrain/kmc_particle.cpp
@@ -1,4 +1,4 @@
-#include "../../include/kmccoursegrain/kmc_particle.hpp"
+#include "../../include/kmccoursegrain/kmc_walker.hpp"
 #include <cassert>
 #include <stdexcept>
 
@@ -9,26 +9,26 @@ namespace kmccoursegrain {
 /****************************************************************************
  * External Methods
  ****************************************************************************/
-KMC_Particle::KMC_Particle() {
+KMC_Walker::KMC_Walker() {
   current_site_ = constants::unassignedId;
   potential_site_ = constants::unassignedId;
   dwell_time_ = -1.0;
 }
 
-int KMC_Particle::getIdOfSiteCurrentlyOccupying() const {
+int KMC_Walker::getIdOfSiteCurrentlyOccupying() const {
   if (current_site_ == constants::unassignedId) {
     throw runtime_error(
         "Cannot get current site as it has not yet been "
-        "assigned. You many need to first initialize the particle");
+        "assigned. You many need to first initialize the walker");
   }
   return current_site_;
 }
 
-int KMC_Particle::getPotentialSite() const {
+int KMC_Walker::getPotentialSite() const {
   if (potential_site_ == constants::unassignedId) {
     throw runtime_error(
         "Cannot get potential site as it has not yet been "
-        "assigned. You many need to first initialize the particle");
+        "assigned. You many need to first initialize the walker");
   }
   return potential_site_;
 }
