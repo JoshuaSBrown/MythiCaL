cb23dab6b945d8fd9a75ebb1209c3a2d4638c6e9
JoshuaSBrown <joshbro42867@yahoo.com>
2020-10-21 08:31:49 -0600

    Added to example1

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -1,126 +1,38 @@
+
+#include <mythical/charge_transport/cubic_lattice.hpp>
+
 #include <cassert> 
 #include <vector>
 #include <random>
 
 using namespace std;
 
-class Lattice{
-  public:
-    Lattice() {};
-
-    Lattice(const int length, 
-        const int width, 
-        const int height,
-        const double mean, 
-        const double std_deviation
-        ) {
-      setNormalDistribution(mean, std_deviation);
-      setDimensions(length, width, height);
-      setRandomNumberGeneratorsForXPlane();
-      assignEnergies();
-    };
-
-    ~Lattice(){};
-
-    void setNormalDistribution(const double mean, const double std_deviation){
-      mean_ = mean;
-      std_deviation_ = std_deviation;
-    }
-
-    void setDimensions(const int length, const int width, const int height) {
-      length_ = length;
-      width_ = width;
-      height_ = height;
-      energies_.resize(length*width*height);
-    }
-
-    void setRandomNumberGeneratorsForXPlane(){
-      assert(height_>0 && width_>0);
-      auto distribution_y_ = std::uniform_int_distribution<int>(0,width_-1);
-      auto distribution_z_ = std::uniform_int_distribution<int>(0,height_-1);
-    }
-
-    void setInterSiteDistance(const double distance){
-      inter_site_distance_ = distance;
-    }
-
-    int getLength() const noexcept { return length_; }
-    int getWidth() const noexcept { return width_; }
-    int getHeight() const noexcept { return height_; }
-
-    double getInterSiteDistance() const noexcept { return inter_site_distance_;}
-
-    double getSiteEnergy(const int index) const noexcept {
-      return energies_.at(index);
-    }
-
-    int getIndex(const int x, const int y, const int z) const noexcept {
-      int y_p = getYPeriodic_(y);
-      int z_p = getZPeriodic_(z);
-      return (z_p*length_*width_)+(y_p*length_)+x;
-    }
-
-    int getIndex(const vector<int> & site_position) const noexcept {
-      return getIndex(site_position.at(0),
-          getYPeriodic_(site_position.at(1)),
-          getZPeriodic_(site_position.at(2)));
-    }
-
-    int getRandomSiteOnXPlane(const int x_plane) {
-      assert(x_plane>=0 && x_plane<length_);
-      int y_lattice_position = distribution_y_(generator_);
-      int z_lattice_position = distribution_z_(generator_);
-      return getIndex(x_plane,y_lattice_position,z_lattice_position);
-    }
-
-    vector<int> getPosition(const int index) const noexcept {
-      int local_index = index; 
-      int z = index / (length_*width_);
-      local_index -= (length_*width_*z);
-      int y = local_index / (length_);
-      int x = local_index % length_;
-      return vector<int>{x,y,z};
-    }
-
-    void assignEnergies(){
-      default_random_engine generator;
-      normal_distribution<double> distribution(mean_,std_deviation_);
-      for(size_t index = 0;index<energies_.size();++index){
-        energies_.at(index) = distribution(generator);
-      }
-    }
-
-  private:
-    int length_ = 0;
-    int width_ = 0;
-    int height_ = 0;
-    double mean_ = 0.0;
-    double std_deviation_ = 0.0;
-    double inter_site_distance_ = 1.0; // nm nanometers
-    vector<double> energies_; // eV electron volts
-
-    uniform_int_distribution<int> distribution_y_;
-    uniform_int_distribution<int> distribution_z_;
-    default_random_engine generator_;
-
-    int getZPeriodic_(const int z) const noexcept {
-      if(z<0){
-        return z+(z/height_)*-1*height_+height_;
-      }
-      return z % height_;
-    }
-
-    int getYPeriodic_(const int y) const noexcept {
-      if(y<0){
-        return y+width_+ (y/width_)*-1*width_;
-      }
-      return y % width_;
-    }
-};
+namespace myla = mythical::lattice;
 
 int main() {
 
- 
+  const int len = 100;
+  const int wid = 50;
+  const int hei = 50;
+  const double inter_site_dist = 1.0;
+  myla::BoundarySetting x_b = myla::BoundarySetting::Fixed;
+  myla::BoundarySetting y_b = myla::BoundarySetting::Periodic;
+  myla::BoundarySetting z_b = myla::BoundarySetting::Periodic;
+
+  myla::Cubic lattice(len, wid, hei, inter_site_dist, x_b, y_b, z_b);
+
+  const int total_num_sites = len * wid * hei; 
+  std::vector<double> site_energies(total_num_sites);
+
+  const double mean = -1.2;
+  const double std_deviation = 0.07;
+
+  std::normal_distribution<double> distribution(mean, std_deviation);
+  
+  std::default_random_engine generator;
+  for(size_t index = 0; index < total_num_sites; ++index){
+    site_energies.at(index) = distribution(generator);
+  }
 
   return 0;
 }
