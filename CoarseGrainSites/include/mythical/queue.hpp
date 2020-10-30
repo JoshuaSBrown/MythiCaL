4de43c7fd46378082e6bb3c7c15744b753685edf
JoshuaSBrown <joshbro42867@yahoo.com>
2020-10-30 09:22:26 -0600

    Adding functionality to the queue

--- CoarseGrainSites/include/mythical/queue.hpp
+++ CoarseGrainSites/include/mythical/queue.hpp
@@ -4,7 +4,7 @@
 #include "constants.hpp"
 
 #include <cstddef>
-#include <list>
+#include <deque>
 
 namespace mythical {
 
@@ -20,17 +20,36 @@ class Queue {
   std::pair<int,double> pop_current();
 
   /**
-   * \brief add to the queue
+   * \brief add to the queue, without sorting
    **/
   void add(std::pair<int,double> walker);
+
+  /**
+   * @brief The walker is added in the correct order.
+   *
+   * @param walker
+   */
+  void sortedAdd(std::pair<int,double> walker);
+
+  bool isSorted() const noexcept;
+
+  void sort(); 
 
   std::size_t size();
  private:
   /**
    * The interger in the pair is the id of the walker and the double is global
-   * dwell time of the walker
+   * dwell time of the walker.
+   *
+   * The Deque has been chosen because will primarily be popping from the front
+   * which is faster than a list and not possible for a vector, also will
+   * require sorting, we do not need to worry about invalidating pointers either
+   * which means a list is not needed. 
+   *
    **/
-  std::list<std::pair<int,double>> walker_queue_;
+  std::deque<std::pair<int,double>> walker_queue_;
+
+  bool sorted_;
 };
 }
 #endif  // MYTHICAL_QUEUE_HPP
